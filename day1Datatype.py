1e6f2e1c77377e9851d26e640ab55fa99ee096f1
Lord Drake Baza <99182490+AeKard@users.noreply.github.com>
2023-02-14 20:10:11 +0800

    DataTypes

---
+++ day1Datatype.py
@@ -0,0 +1,30 @@
+import random
+#----------------------------------------------------
+# DATA TYPE - storing value
+# string, char
+sentence = "Hello World!"
+char = 'c'
+# integer, float
+wholeNumber = 12
+numberFloat = 12.12
+# Bollean value
+TRUE = True
+FALSE =  False
+# ARRAY
+sentence = "HELLO WORLD!"
+array = ["H","E","L","L","O"," ","W","O","R","L","D","!"]
+    #   { 0 , 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10, 11}
+mutableArraySET1 = {1,2,3,5,6,7,8}  #changable SET
+mutableArrayList2 = [1,2,3,5,6,7,8] #changable LIST
+immutableArrayTuple = (1,2,3,4,5) # unchangable tuple
+Dict = {"username" : "Mariella",2: "Female"}  # changable
+Dict["username"] = "Biscarra"
+
+len(sentence) # LEGNTH <-- 
+type(sentence) # Returns data type
+bool(numberFloat)
+20e2
+# Conversion 
+2 + int("2")
+# range(START,END)
+random.randrange(0, 10)
