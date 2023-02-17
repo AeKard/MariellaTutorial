f31ce6c8ee0d511579d5ce0df24c959d76293d7a
Lord Drake Baza <99182490+AeKard@users.noreply.github.com>
2023-02-17 13:42:38 +0800

    Add files via upload

---
+++ day2Strings.py
@@ -0,0 +1,56 @@
+#------------------------------------------------
+name = """MULTILINE
+
+    MY Name is Marilla,
+"""
+myfullname = "Mariella Biscarra"
+# Question 
+# print(f"count: {len(myfullname)} , middle Letter : {myfullname[int(len(myfullname) / 2)]} half:  {len(myfullname)/ 2}")
+# print(f"lastname: {myfullname[int(len(myfullname)/ 2):]}, index : {int(len(myfullname)/ 2)}")
+# odd 1,3,5,7,9,11
+# even 0,2,4,5,6,8,10
+b = "Hello World! this jelp rarp"
+# print(b[-5:-2])
+
+# Method in print statment string manipulation
+    #1. lower(), upper()
+    #2. strip()
+    #3. replace()
+    #4. split()
+
+# string format
+# print("My name is {} and I am a {} and I am currently learning {}".format("Marriella","Female", "Python"))
+print("Python\\somehitng")
+
+# NAMING CONVESION
+# CAMEL CASE
+listOfNames = ["names"]
+# Snake Case
+list_of_Names = ["names"]
+
+# One dimension array
+#   [1,2,3,4,5]
+# 2dimensional Array
+#[    0 1 2 3 4
+#   0[1],2,3,4,5]
+#   1[1,2,3,4,5]
+#   2[1,2,4,5,6]
+# ]
+
+# Multidimesional Array
+#[
+#   0[
+#       0[1,2,3,4,5]
+#   ],
+#   1[
+#       1[1,2,3,4,5]
+#   ]
+# 
+# ]
+
+#   Jagged array
+#[
+#   [1,23,,4,5,6,6]
+#   [1,2,3,4,5,6]
+#   [2,3,4,5]
+# ]
---
+++ day3Array.py
@@ -0,0 +1,38 @@
+# ARRAY IN PYTHON
+#
+#   List - is a collection whihc is {ordered and changeable}. allows {duplicate memebrs}
+#   Tuple - is a collection which is {ordered and unchangeable}. Allows {duplicate members.}
+#   Set - is s a collection which is unordered, unchangeable*, and unindexed. No duplicate members.
+#   Dictionary - is a collection which is ordered** and changeable. No duplicate members.
+
+# what is variablename, what is the index of that variable that you want to replace
+print("=" * 40)
+ListOfNames = ["Venedict", "LordDrake", "Mariella", "Marvin"]
+print(type(ListOfNames))
+# variable name [index] = you want to replace
+ListOfNames[1] = "Colleen"
+ListOfNames.insert(0,"anothername")
+# Add from backoftheline
+ListOfNames.append("Lord Drake")
+print(ListOfNames)
+ListOfNames.sort()
+print(ListOfNames)
+
+print("=" * 40)
+TupleOfnames = {"VEnedict", "LOrddrake", "mariella", "marvin", "marvin"}
+# TupleOfnames[1] = "Colleen"
+print(type(TupleOfnames))
+TupleOfnames.add("Budoy")
+TupleOfnames.discard("LOrddrake")
+print(TupleOfnames)
+
+dict_students = {
+    # KEY : "VARIABLE"
+    1: {"name": "Lord Drake", "Gender": "Male", "GradeLevel" : "Firstyear"},
+    2: {"name": "Mariella Biscarra", "Gender": "Female", "GradeLevel" : "Firstyear"}
+}
+print(dict_students[1]["name"])
+print(dict_students.keys())
+
+
+
